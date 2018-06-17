11675c9eeb989491122b7a34af3e6c009b50e671
Michał Kawecki <michycfc@gmail.com>
2018-06-17 13:16:27 +0200

    xD

--- src/main/java/pl/edu/wat/AppRunner.java
+++ src/main/java/pl/edu/wat/AppRunner.java
@@ -5,10 +5,8 @@ import pl.edu.wat.algorithm.AlgorithmManager;
 public class AppRunner {
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            throw new IllegalArgumentException("You must pass data argument");
-        }
-        AlgorithmManager algorithmManager = new AlgorithmManager(Data.valueOf(String.valueOf(args[0]).toUpperCase()));
+        String data = "file";
+        AlgorithmManager algorithmManager = new AlgorithmManager(Data.valueOf(data.toUpperCase()));
         algorithmManager.run();
     }
 }
